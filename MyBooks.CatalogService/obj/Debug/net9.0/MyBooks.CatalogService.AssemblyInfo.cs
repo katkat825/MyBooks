5f5866ff4d5a37d802724d8dd37f1cf7665a47fe
katkat825 <137170306+katkat825@users.noreply.github.com>
2025-03-06 17:13:48 -0500

    fixed spine height bug

--- MyBooks.CatalogService/obj/Debug/net9.0/MyBooks.CatalogService.AssemblyInfo.cs
+++ MyBooks.CatalogService/obj/Debug/net9.0/MyBooks.CatalogService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyBooks.CatalogService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7d8f661952d783b7e18ad9911a174cc7fd8efa9")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyBooks.CatalogService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyBooks.CatalogService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
