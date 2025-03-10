1ca99cbe33afc0c3bf1dfdfe2fff0a4085ba1f2a
katkat825 <137170306+katkat825@users.noreply.github.com>
2025-03-10 13:45:33 -0400

    add search function to book-list

--- MyBooks.UI/src/app/components/book-list/book-list.component.ts
+++ MyBooks.UI/src/app/components/book-list/book-list.component.ts
@@ -1,21 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import { BookService } from '../../services/book.service';
 import { CommonModule } from '@angular/common';
-import { dateTimestampProvider } from 'rxjs/internal/scheduler/dateTimestampProvider';
-import { Router, RouterModule, RouterOutlet } from '@angular/router';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
 import { HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { Router } from '@angular/router';
+import { BookService } from '../../services/book.service';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-book-list',
   standalone: true,
   templateUrl: './book-list.component.html',
   styleUrl: './book-list.component.css',
-  imports: [CommonModule, MatIconModule, MatButtonModule, HttpClientModule]
+  imports: [CommonModule, MatIconModule, MatButtonModule, HttpClientModule, MatFormFieldModule, MatInputModule, FormsModule]
 })
 export class BookListComponent {
   books: any[] = [];
+  filteredBooks: any[] = [];
+  searchQuery: string = '';
 
   constructor(private bookService: BookService, private router: Router) { }
 
@@ -32,10 +36,19 @@ export class BookListComponent {
       next: (data) => {
         console.log("Books received in UI: ", data);
         this.books = data;
+        this.filteredBooks = data;
       },
       error: (error) => console.error('Error loading books', error),
       complete: () => console.log("Books fetch completed.")
     });
+  }
+
+  filterBooks() {
+    const query = this.searchQuery.toLowerCase().trim();
+    this.filteredBooks = this.books.filter(book =>
+      book.title.toLowerCase().includes(query) ||
+      (book.author && book.author.toLowerCase().includes(query))
+    );
   }
 
   addBook() {
