0d8fae2bfdd25d5289ec7ddb4cd52ddf3179cf6a
katkat825 <137170306+katkat825@users.noreply.github.com>
2025-08-19 21:35:04 -0400

    add dockerfiles

--- MyBooks.CatalogService/obj/Debug/net9.0/MyBooks.CatalogService.AssemblyInfo.cs
+++ MyBooks.CatalogService/obj/Debug/net9.0/MyBooks.CatalogService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyBooks.CatalogService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24246aab4ea1d0c522c3549315ae7f44b541d362")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5dd675f314706bcc89151741434f39a48692f4a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyBooks.CatalogService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyBooks.CatalogService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
