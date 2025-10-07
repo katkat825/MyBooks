f88cb5e28805f7a518a1e57d469112ab433db8e9
katkat825 <137170306+katkat825@users.noreply.github.com>
2025-10-07 14:12:54 -0400

    prep for docker

--- MyBooks.UI/src/environments/environment.prod.ts
+++ MyBooks.UI/src/environments/environment.prod.ts
@@ -1,6 +1,12 @@
 export const environment = {
-  production: true,
+  production: false,
   apiUrl: 'https://localhost:5001/api', // CatalogService
   authServiceUrl: 'https://localhost:7254/api', // AuthService
-  fileApiUrl: `https://localhost:7142/api/files` //FileService
+  fileApiUrl: `https://localhost:7142/api/files`, // FileService
+  tenantApiUrl: `https://localhost:5005/api`, // TenantService
+  integrationApiUrl: `https://localhost:7142/api`,
+  authBaseUrl: 'https://localhost:7254',
+  emailServiceUrl: 'https://localhost:5006/api',
+  fileBaseUrl: 'https://localhost:7142',
+  supportBaseUrl: 'https://localhost:7221'
 };
