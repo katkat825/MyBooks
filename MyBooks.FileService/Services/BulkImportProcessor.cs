a0ae3884f4c89d63ee2bcdff98d2b180012c9768
katkat825 <137170306+katkat825@users.noreply.github.com>
2025-11-11 08:05:18 -0500

    #124 - rethink conversion

--- MyBooks.FileService/Services/BulkImportProcessor.cs
+++ MyBooks.FileService/Services/BulkImportProcessor.cs
@@ -229,7 +229,7 @@ public class BulkImportProcessor
 
                 item.CreatedFileId = fileMeta.Id;
 
-                
+                /* looks like crap
                 // convert to epub 
                 if (file.MimeType == "application/pdf")
                 {
@@ -257,7 +257,7 @@ public class BulkImportProcessor
                         Console.WriteLine($"[BulkImportProcessor] Error during conversion/upload for {item.FileName}: {ex.Message}");
                     }
                 }
-                
+                */
 
                 var linkDto = new BookFileLinkDto
                 {
