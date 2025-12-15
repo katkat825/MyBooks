4b7809ca8d25b86655e196770985f52f735f6ef4
katkat825 <137170306+katkat825@users.noreply.github.com>
2025-12-15 17:07:22 -0500

    new full qa env on new server

--- MyBooks.UI/src/environments/environment.qa.ts
+++ MyBooks.UI/src/environments/environment.qa.ts
@@ -2,11 +2,11 @@ export const environment = {
   production: true,
   environmentName: 'qa',
 
-  catalogBaseUrl: 'https://mybookcatalog.com/api/catalog',
-  tenantBaseUrl: 'https://mybookcatalog.com/api/tenant',
-  authBaseUrl: 'https://mybookcatalog.com/api/auth',
-  emailBaseUrl: 'https://mybookcatalog.com/api/email',
-  fileBaseUrl: 'https://mybookcatalog.com/api/file',
-  supportBaseUrl: 'https://mybookcatalog.com/api/support',
+  catalogBaseUrl: 'https://qa.mybookcatalog.com/api/catalog',
+  tenantBaseUrl: 'https://qa.mybookcatalog.com/api/tenant',
+  authBaseUrl: 'https://qa.mybookcatalog.com/api/auth',
+  emailBaseUrl: 'https://qa.mybookcatalog.com/api/email',
+  fileBaseUrl: 'https://qa.mybookcatalog.com/api/file',
+  supportBaseUrl: 'https://qa.mybookcatalog.com/api/support',
   googlePickerApiKey: 'AIzaSyCDhTaYWXXx_qkyxuXYwDjrLJXfEchQJ9Q'
 };
